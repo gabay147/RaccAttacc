6de3726c1c289cafdeda9b02996fdfe3c3b38654
Pappadopalus <112454733+Pappadopalus@users.noreply.github.com>
2022-10-10 20:36:12 -0500

    created physics and movement functions

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -1,24 +1,33 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# physics variables
+var vel : Vector2 = Vector2()
+var jumpforce : int = 600
+var gravity : int = 800
+const speed : int = 200
+var facingDir : int = 1
 
+
+
+# physics
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1.0
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1.0
-	#if Input.is_action_pressed( J U M P ):
-	#	JUMP BEHAVIOR
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	
+	vel.x = 0
+	
+		# movement inputs
+	if Input.is_action_pressed("A"):
+		vel.x -= speed
+	if Input.is_action_pressed("D"):
+		vel.x += speed
+		
+	
+	# applying the velocity
+	vel = move_and_slide(vel, Vector2.UP)
+	
+	# gravity
+	vel.y += gravity * delta
+	
+	# jump input
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		vel.y -= jumpforce
