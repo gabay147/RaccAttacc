bbf5e3156c1910d644df2c8e247ad8e3f2c19ee2
Pappadopalus <112454733+Pappadopalus@users.noreply.github.com>
2022-10-10 21:44:34 -0500

    Update PlayerCharacter.gd

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
 
-
+# player state variables
+onready var ladder_state = false
+onready var attack_state = false
 
 # sprites and animation variables
 onready var sprite : Sprite = get_node("Sprite")
@@ -14,18 +16,19 @@ var gravity : int = 800
 const speed : int = 200
 var facingDir : int = 1
 
-
-
 # physics
 func _physics_process(delta):
 	
 	vel.x = 0
 	
-	# movement inputs
-	if Input.is_action_pressed("left"):
-		vel.x -= speed
-	if Input.is_action_pressed("right"):
-		vel.x += speed
+	#Player State - Ground
+	moveGround()
+	
+	#Player State - Ladder
+	#moveLadder()
+	
+	#Player State - Shoot
+	#moveShoot()
 	
 	
 	# interact
@@ -42,14 +45,43 @@ func _physics_process(delta):
 	# jump input
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		vel.y -= jumpforce
+
+
+func moveGround():
 	
-	# sprite direction
-	if vel.x < 0:
-		sprite.flip_h = true
+	
+	# movement inputs
+	if Input.is_action_pressed("left"):
+		vel.x -= speed
 		facingDir = 1
-	elif vel.x > 0:
-		sprite.flip_h = false
+		print("left")
+	if Input.is_action_pressed("right"):
+		vel.x += speed
 		facingDir = -1
+		print("right")
+	
+	
+	# jump input
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		vel.y -= jumpforce
+  #Player should be able to...
+  # 3) shoot
+
+func moveLadder():
+	pass
+	
+  #Get user input and move
+  #Player should be able to...
+  # 1) move up and down
+  # 2) change facingDir (move left/right, will change sprite)
+  # 3) jump off the ladder (jump input, movement behavior will be based on facingDir
+  # Also include important variable tracking like facingDir
+
+#func moveShoot():
+  #Get user input (not sure what that will look like)
+  #Player should be able to...
+  # 1) Wait for cooldown? shoot more and launch self infinitely? Will need to play with this mechanic!
+
 
 
 # called when you die
