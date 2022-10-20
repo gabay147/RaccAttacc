a91b2c549f483788e44e1e844c7ff276f295ad16
Pappadopalus <112454733+Pappadopalus@users.noreply.github.com>
2022-10-19 22:00:37 -0500

    Update Bullet.gd

--- Bullet.gd
+++ Bullet.gd
@@ -5,8 +5,9 @@ extends Area2D
 var speed : float = 30.0
 var damage : int = 1
 
-#func _process(delta):
-#	translation += global_transform.basis.x * speed * delta
+func _process(delta):
+	#global_transform += global_transform.basis_xform(Vector2(1,0))
+	#use move_and_collide
 
 
 
